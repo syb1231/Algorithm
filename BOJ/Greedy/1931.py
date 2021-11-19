7c6219ab1a3e773dd4c5bf8dbd7863b4c2e74673
syb1231 <64253285+syb1231@users.noreply.github.com>
2021-11-19 22:36:07 +0900

    Create 1931.py

---
+++ BOJ/Greedy/1931.py
@@ -0,0 +1,20 @@
+import sys
+
+
+N = int(sys.stdin.readline()) 
+time = [[0]*2 for _ in range(N)]
+for index in range(N):
+    a, b = map(int, sys.stdin.readline().split()) 
+    time[index][0] = a 
+    time[index][1] = b
+
+time.sort(key=lambda x : (x[1], x[0]))
+
+lastTime=-1
+count=0
+for index in range(N):
+    if(lastTime<=time[index][0]):
+        count += 1
+        lastTime = time[index][1]
+
+print(count)
