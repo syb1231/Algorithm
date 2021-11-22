24bc2b772d078231b05843e60383ea743424e819
syb1231 <64253285+syb1231@users.noreply.github.com>
2021-11-22 14:46:58 +0900

    Silver3 / DP
    Silver3 / DP

---
+++ BOJ/DP/1463.py
@@ -0,0 +1,23 @@
+import sys
+def dp(d,num):
+
+    return d[num]
+        
+
+N= int(sys.stdin.readline()) 
+d = [-1 for _ in range(N+1)]
+d[0]=0
+d[1]=0
+for num in range (2,len(d)):
+    # if(d[num] != -1) :
+    #     return d[num]
+    a0 = 1000000
+    a1 = 1000000
+    if((num % 2) == 0) :
+        a0 = d[num//2]
+        
+    if((num % 3) == 0) :
+        a1 = d[num//3]
+    a2 = d[num-1]
+    d[num] = min(a0,a1,a2) + 1    
+print(d[N])
