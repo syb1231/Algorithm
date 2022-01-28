506b40ee79c1ab7f213497315361942aea82a143
syb1231 <64253285+syb1231@users.noreply.github.com>
2022-01-28 18:07:31 +0900

    Gold5 / DP / 답지봄
    Gold5 / DP / 답지봄

---
+++ BOJ/DP/12865.py
@@ -0,0 +1,28 @@
+from collections import deque
+from itertools import combinations
+import sys
+import copy
+input = sys.stdin.readline
+sys.setrecursionlimit(10000000)
+
+n,k = map(int,input().split())
+info = []
+
+for i in range(n):
+    info.append(list(map(int,input().split())))
+
+d = [[0]*(k+1) for _ in range(n)]
+for i in range(1,k+1):
+    if(info[0][1]>i):
+        continue
+    else:
+        d[0][i] = info[0][1]
+
+for i in range(n):
+    for j in range(1,k+1):
+        if(info[i][0] > j):
+            d[i][j] = d[i-1][j]
+        else:
+            d[i][j] = max(info[i][1] + d[i-1][j-info[i][0]],d[i-1][j])
+print(d[n-1][k])
+# 초항을 세워서 확인해 보자 점화식 만들기 개 어렵네
