d6729ffbf3b5a69e8f94dec11c536415a607ebed
syb1231 <64253285+syb1231@users.noreply.github.com>
2022-02-08 20:59:37 +0900

    Gold5 / pq
    Gold5 / pq

---
+++ BOJ/etc/2075.py
@@ -0,0 +1,17 @@
+from collections import deque
+from itertools import combinations
+import sys
+import copy
+import heapq
+input = sys.stdin.readline
+pq = []
+n = int(input())
+arr = []
+for i in range(n):
+    arr = list(map(int,input().split()))
+    for j in arr:
+        heapq.heappush(pq,j)
+        if(len(pq)>n):
+            heapq.heappop(pq)
+    
+print(heapq.heappop(pq))
